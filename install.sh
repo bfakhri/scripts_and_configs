735ea75c5c3f03d818b079abcd107ef94a035047
Bijan Fakhri <bfakhri@asu.edu>
2017-08-28 10:04:29 -0700

    install.sh now clones the vim indent guides repo and copies the directors into the .vim/ dir installing the plugin

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 cp ./scripts/vimrc ~/.vimrc
+# Install vim indent guides
+git clone https://github.com/nathanaelkane/vim-indent-guides.git
+cp -r vim-indent-guides/autoload/ ~/.vim/
+cp -r vim-indent-guides/plugin/ ~/.vim/
+cp -r vim-indent-guides/doc/ ~/.vim/
+rm -rf vim-indent-guides/
 sudo cp ./scripts/mouseacc.sh /etc/profile.d/mouseacc.sh
 cp ./scripts/i3_config ~/.i3/config
 sudo cp ./scripts/i3status.conf /etc/i3status.conf 
