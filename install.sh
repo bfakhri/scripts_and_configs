21a75b160cea083f5e14a09df46b5fd6093b220f
Bijan Fakhri <bfakhri@asu.edu>
2018-08-30 11:52:34 -0700

    Install redshift

--- install.sh
+++ install.sh
@@ -9,3 +9,5 @@ rm -rf vim-indent-guides/
 sudo cp ./scripts/mouseacc.sh /etc/profile.d/mouseacc.sh
 cp ./scripts/i3_config ~/.i3/config
 sudo cp ./scripts/i3status.conf /etc/i3status.conf 
+# Install Redshift
+sudo apt-get install redshift-gtk
