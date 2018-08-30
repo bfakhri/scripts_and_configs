5c75c123cae256edeee8e12f3c82c1de303b364c
Bijan Fakhri <bfakhri@asu.edu>
2018-08-30 11:51:20 -0700

    u

--- scripts/mouseacc.sh
+++ scripts/mouseacc.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-for id in `xinput --list|grep 'Logitech Gaming Mouse G502'|perl -ne 'while (m/id=(\d+)/g){print "$1\n";}'`; do
-#for id in `xinput --list|grep 'Logitech USB Optical Mouse'|perl -ne 'while (m/id=(\d+)/g){print "$1\n";}'`; do
+#for id in `xinput --list|grep 'Logitech Gaming Mouse G502'|perl -ne 'while (m/id=(\d+)/g){print "$1\n";}'`; do
+for id in `xinput --list|grep 'Logitech USB Optical Mouse'|perl -ne 'while (m/id=(\d+)/g){print "$1\n";}'`; do
     # echo "setting device ID $id"
     notify-send -t 50000  'Mouse fixed'
     xinput set-prop $id 'Device Accel Profile' -1
