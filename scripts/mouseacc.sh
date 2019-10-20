8e78e5efdc5a2def63619282765904062e9d21e6
Bijan Fakhri <bfakhri@asu.edu>
2019-10-20 12:33:39 -0400

    Fixed for Ubuntu 18.04

--- scripts/mouseacc.sh
+++ scripts/mouseacc.sh
@@ -2,7 +2,9 @@
 #for id in `xinput --list|grep 'Logitech Gaming Mouse G502'|perl -ne 'while (m/id=(\d+)/g){print "$1\n";}'`; do
 for id in `xinput --list|grep 'Logitech USB Optical Mouse'|perl -ne 'while (m/id=(\d+)/g){print "$1\n";}'`; do
     # echo "setting device ID $id"
-    notify-send -t 50000  'Mouse fixed'
-    xinput set-prop $id 'Device Accel Profile' -1
-    xinput set-prop $id "Device Accel Constant Deceleration" 1.0
+    notify-send -t 50000  'Fixing Mouse'
+    #xinput set-prop $id 'Device Accel Profile' -1
+    #xinput set-prop $id "Device Accel Constant Deceleration" 1.0
+    xinput set-prop $id 'libinput Accel Speed' -0.5
+    notify-send -t 50000  'Mouse Fixed'
 done 
