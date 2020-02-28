4942d54e70fb80ed7b043e8834dce941f8774d0c
Bijan Fakhri <bfakhri@asu.edu>
2020-02-28 09:59:56 -0700

    Added unmute script

--- install.sh
+++ install.sh
@@ -6,3 +6,5 @@ cp ./scripts/i3_config ~/.i3/config
 sudo cp ./scripts/i3status.conf /etc/i3status.conf 
 # Install Redshift
 sudo apt-get install redshift-gtk
+# Install unmute script 
+cp ./scripts/unmute.sh ~/unmute.sh
