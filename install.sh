2b4213cb55235bc75db03a4a7e4ec2505171d13d
Bijan Fakhri <bijan.fakhri@statsperform.com>
2020-07-31 13:14:54 -0500

    DPI + cleanup

--- install.sh
+++ install.sh
@@ -1,10 +1,31 @@
 #!/bin/sh
-cp ./scripts/vimrc ~/.vimrc
-sudo cp ./scripts/mouseacc.sh /etc/profile.d/mouseacc.sh
-sudo cp ./scripts/touch_screen.sh /etc/profile.d/touch_screen.sh
-cp ./scripts/i3_config ~/.i3/config
+
+## INSTALL CONFIG FILES ##
+
+# i3 configuration
+cp ./scripts/i3_config ~/.config/i3/config
+
+# i3_status configuration
 sudo cp ./scripts/i3status.conf /etc/i3status.conf 
+
+# vim config
+cp ./scripts/vimrc ~/.vimrc
+
+# Xresouces (for DPI)
+cp ./scripts/Xresources ~/.Xresources
+
+
+
+## INSTALL PACKAGES ##
 # Install Redshift
 sudo apt-get install redshift-gtk
-# Install unmute script 
-cp ./scripts/unmute.sh ~/unmute.sh
+
+
+## OLD SCRIPTS ##
+
+## Unmute script 
+#cp ./scripts/unmute.sh ~/unmute.sh
+#sudo cp ./scripts/touch_screen.sh /etc/profile.d/touch_screen.sh
+#
+## Mouse acceleration
+#sudo cp ./scripts/mouseacc.sh /etc/profile.d/mouseacc.sh
